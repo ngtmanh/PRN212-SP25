7abe0b2f7e6fe802eee99d09540233e405876f9a
Dungpt <phamtiendung508@gmail.com>
2025-03-15 12:29:17 +0700

    Cập nhật phần login và chỉnh sửa dữ liệu cá nhân Citizen

---
+++ Prn212/CitizenProfile.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Prn212.Models;
+
+namespace Prn212
+{
+    public partial class CitizenProfile : Window
+    {
+        private Prn212Context _context = new Prn212Context();
+        private User _currentUser;
+
+        public CitizenProfile(User user)
+        {
+            InitializeComponent();
+            _currentUser = user;
+            LoadUserData();
+        }
+
+        private void LoadUserData()
+        {
+            txtFullName.Text = _currentUser.FullName;
+            txtEmail.Text = _currentUser.Email;
+            txtAddress.Text = _currentUser.Address;
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            var userToUpdate = _context.Users.FirstOrDefault(u => u.UserId == _currentUser.UserId);
+            if (userToUpdate != null)
+            {
+                userToUpdate.FullName = txtFullName.Text;
+                userToUpdate.Email = txtEmail.Text;
+                userToUpdate.Address = txtAddress.Text;
+
+                if (!string.IsNullOrEmpty(txtPassword.Password))
+                {
+                    userToUpdate.Password = txtPassword.Password; 
+                }
+
+                _context.SaveChanges();
+                MessageBox.Show("Cập nhật thành công!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); 
+        }
+    }
+}
---
+++ Prn212/login.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Prn212.Models;
+
+namespace Prn212
+{
+    public partial class Login : Window
+    {
+        private readonly Prn212Context _context;
+
+        public Login()
+        {
+            _context = new Prn212Context();
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Password;
+
+            var user = _context.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
+
+            if (user != null)
+            {
+                MessageBox.Show("Login successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+                if (user.Role == "AreaLeader")
+                {
+                    MainWindow window = new MainWindow();
+                    window.Show();
+                }
+
+                if (user.Role == "Citizen")
+                {
+                    CitizenWindow citizenWindow = new CitizenWindow(user);
+                    citizenWindow.Show();
+                }
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid email or password.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BtnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Register registerWindow = new Register(); 
+            registerWindow.ShowDialog(); 
+        }
+    }
+}
+
