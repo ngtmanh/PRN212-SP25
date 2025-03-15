5b3ed3ff79ce199d1832a219b4e70debb6af3f6b
manhnt57 <manhnt57@fpt.com>
2025-03-15 15:00:18 +0700

    quản lý member

---
+++ Prn212/Resource/ConstLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prn212.Resource
+{
+    public static class ConstLog
+    {
+        public const string HOUSE_HOLD_MEMBER_ADD = "HOUSE_HOLD_MEMBER_ADD";
+        public const string HOUSE_HOLD_MEMBER_ADD_SUCCESS = "HOUSE_HOLD_MEMBER_ADD_SUCCESS";
+        public const string HOUSE_HOLD_MEMBER_ADD_FAIL = "HOUSE_HOLD_MEMBER_ADD_FAIL";
+
+        public const string HOUSE_HOLD_MEMBER_UPDATE = "HOUSE_HOLD_MEMBER_UPDATE";
+        public const string HOUSE_HOLD_MEMBER_UPDATE_SUCCESS = "HOUSE_HOLD_MEMBER_UPDATE_SUCCESS";
+        public const string HOUSE_HOLD_MEMBER_UPDATE_FAIL = "HOUSE_HOLD_MEMBER_UPDATE_FAIL";
+
+        public const string HOUSE_HOLD_MEMBER_DELETE = "HOUSE_HOLD_MEMBER_DELETE";
+        public const string HOUSE_HOLD_MEMBER_DELETE_SUCCESS = "HOUSE_HOLD_MEMBER_DELETE_SUCCESS";
+        public const string HOUSE_HOLD_MEMBER_DELETE_FAIL = "HOUSE_HOLD_MEMBER_DELETE_FAIL";
+
+    }
+}
